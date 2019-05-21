e0467216b0459845d0b90b9de63b99a292167551
MaryJay787 <josephmaryam.mj@gmail.com>
2019-05-21 14:30:44 -0400

    Enumerable Drill Done

--- lib/enumerable.rb
+++ lib/enumerable.rb
@@ -1,1 +1,42 @@
-#Write your code here
+require "pry"
+
+def using_map(array)
+  array.map {|word| "I love #{word} on my pizza!"}
+end
+
+def using_select(array)
+  array.select do |string|
+    if string.class == String
+      string
+    end
+  end
+end
+
+def using_find(array)
+  # new = array.to_a
+  array.find do |num|
+    if num % 5 == 0 && num % 15 == 0
+      num[0]
+    end
+  end
+end
+
+def using_sort(array)
+  array.sort
+end
+
+def using_max(array)
+  array.max
+end
+
+def using_include(array, element)
+  array.include?(element)
+end
+
+def using_all?(array)
+  array.all? {|num| num.even?}
+end
+
+def using_any?(array)
+  array.any? {|num| num == Integer}
+end
